5b8713d2cdaa1f7330afe33f17d558b952af5501
abhishekomp <abhishek2283@hotmail.com>
2024-11-17 23:05:07 +0100

    display iteration count (serial number) of item in the list on the html template.

--- coffeeshop-frontend/src/main/resources/static/js/orders.js
+++ coffeeshop-frontend/src/main/resources/static/js/orders.js
@@ -12,6 +12,10 @@ document.addEventListener('alpine:init', () => {
             console.log("inside orders.js:getOrders with pageNo: ", pageNo);
             $.getJSON("http://localhost:8082/api/orders?page="+pageNo, (data) => {
                 console.log("Get Order(s) response:", data);
+                for (let i = 0; i < data.length; i++) {
+                  // Runs 5 times, with values of step 0 through 4.
+                  console.log("order created on: ", data[i].createdAt);
+                }
                 this.orders = data;
             });
         }
